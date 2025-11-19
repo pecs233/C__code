b12be0eb531159385643ece5f7175b1065ce06a0
Admin <16225711+beer-tea-shuang_0@user.noreply.gitee.com>
2025-11-19 16:47:25 +0800

    函数练习

---
+++ 6.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int sign(int x)
+//{
+//	if (x > 0)
+//		return 1;
+//	else if (x == 0)
+//		return 0;
+//	else
+//		return -1;
+//}
+//int main()
+//{
+//	int x;
+//	printf("Enter x£º");
+//	scanf("%d", &x);
+//	int r=sign(x);
+//	printf("sign(%d) = %d\n", x,r);
+//	return 0;
+//}
+
+//void Day(year)
+//{
+//	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+//		printf("31 29 31 30 31 30 31 31 30 31 30 31\n");
+//	else
+//		printf("31 28 31 30 31 30 31 31 30 31 30 31\n");
+//}
+//int main()
+//{
+//	int year;
+//	printf("Enter year: ");
+//	scanf("%d", &year);
+//	Day(year);
+//	return 0;
+//}
+//void matrix(int length, int width, char ch)
+//{
+//	for (int i = 0; i < width; i++)
+//	{
+//		for (int j = 0; j < length; j++)
+//		{
+//			printf("%c", ch);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int length,width,ch;
+//	printf("Enter length,width,ch: ");
+//	scanf("%d %d %c", &length, &width, &ch);
+//	matrix(length, width, ch);
+//	return 0;
+//}
