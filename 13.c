f69baf1289a49daf98d7c2607b580e36c25c8c83
pecs233 <931991620@qq.com>
2025-11-27 18:55:31 +0800

    My_strcat函数的实现

---
+++ 13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "assert.h"
+char* My_strcat(char* des, const char* sou)
+{
+	assert(des && sou);
+	char* ret = des;
+	while (*(des))
+	{
+		des++;
+	}
+	while (*des++ = *sou++);
+
+	return ret;
+}
+int main()
+{
+	char arr[30] = { "Hello " };
+	My_strcat(arr, "World");
+	printf("%s", arr);
+	return 0;
+}
