9cd3ab485bd07eecc0b725fb85ee4cb15a2db553
pecs233 <931991620@qq.com>
2025-12-05 15:13:50 +0800

    测试存放在register中的变量

---
+++ 19.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int power(int x, register int n)
+////存放在register里的变量在调用时比一般变量更快,register的变量没有地址
+//{
+//	int p;
+//	for (p = 1;n > 0;n--)
+//		p *= x;
+//	return p;
+//}
+//int main()
+//{
+//	int s;
+//	s = power(5, 3);
+//	printf("%d\n", s);
+//}
